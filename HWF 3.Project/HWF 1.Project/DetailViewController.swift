31cc31c6992eef7099946dbe68786a17e4245842
Levent İrdem <leventirdem@gmail.com>
2023-03-05 00:24:25 +0300

    HWF 3.Project

---
+++ HWF 3.Project/HWF 1.Project/DetailViewController.swift
@@ -0,0 +1,57 @@
+//
+//  DetailViewController.swift
+//  HWF 1.Project
+//
+//  Created by Levent İrdem on 28.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    var selectedImage:String?
+    
+    var selectedImageIndex:Int?
+    var totalImageNumber:Int?
+    
+    
+    @IBOutlet var imageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        if let index = selectedImageIndex, let total = totalImageNumber{
+            title = "Picture \(index) of \(total)"
+        }else {
+            title = selectedImage
+        }
+        
+        navigationItem.largeTitleDisplayMode = .never
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        if let imageToLoad = selectedImage{
+            imageView.image = UIImage(named: imageToLoad)
+        }
+        
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+   
+
+}
