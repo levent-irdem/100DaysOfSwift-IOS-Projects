3941c3dc55b031ef2192c3c338a5bc7aebb0bc5f
Levent İrdem <leventirdem@gmail.com>
2023-03-08 23:09:17 +0300

    HWF 4.Project

---
+++ HWF 4.Project/HWF 4.ProjectUITests/HWF_4_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HWF_4_ProjectUITestsLaunchTests.swift
+//  HWF 4.ProjectUITests
+//
+//  Created by Levent İrdem on 6.03.2023.
+//
+
+import XCTest
+
+final class HWF_4_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
